d965a72917200416591da0e146b742e8fd81b47f
K.Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2022-12-01 14:33:18 +0530

    Create String3.java
    Write a program to copy a string from one variable to other using string library functions: 
    
    INPUT & OUTPUT FORMAT: 
    
    Input consists of 1 string.
    
    Output consists of 1 string.
    
    Sample Input:
    hello 
    
    Sample Output:
    hello

---
+++ String3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class String3
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}
