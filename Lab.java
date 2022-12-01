f2a7bcc748bc7716a47968baf803752abc27d247
K.Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2022-12-01 13:48:24 +0530

    Create Lab.java
    There are 3 labs in the CSE department (L1, L2 and L3) with a seating capacity of x, y and z. find the lab which has the minimal seating capacity. 
    
    Input and Output Format:
    
    Assume that x, y and z are always distinct.
    
    Refer sample input and output. 
    
    Note:
    
    All text in bold corresponds to input and the rest corresponds to output.
    
    Sample Input and Output 1:
    
    Enter x
    
    30
    
    Enter y
    
    40
    
    Enter z
    
    20
    
    L3 has the minimal seating capacity

---
+++ Lab.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Lab
+{
+    public static void main(String args[])
+    {
+        //write your code
+        Scanner sc=new Scanner(System.in);
+        int x,y,z;
+        System.out.println("Enter x");
+        x=sc.nextInt();
+        System.out.println("Enter y");
+        y=sc.nextInt();
+        System.out.println("Enter z");
+        z=sc.nextInt();
+        if(x<y && x<z)
+        {
+            System.out.println("L1 has the minimal seating capacity");
+            
+        }
+        else if(y<z && y<x)
+        {
+        System.out.println("L2 has the minimal seating capacity");
+        }
+        else if(z<x && z<y)
+        {
+            System.out.println("L3 has the minimal seating capacity");
+        }
+        
+        
+    }
+}
