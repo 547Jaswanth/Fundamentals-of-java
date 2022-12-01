6ed760eda65ff072f449c5daae51eae66984c969
K.Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2022-12-01 14:30:01 +0530

    Create String1.java
    Write a program to scan the string from the user and to print it
    
    INPUT  & OUTPUT FORMAT: 
    
    Input consists of 1 string.
    
    Output consists of 1 string.
    
    Sample Input:
    Cyfuno
    
    Sample Output:
    
    The string is Cyfuno

---
+++ String1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class String1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str= sc.nextLine();  
+        System.out.printf("The string is %s",str);
+        
+    }
+}
