e9e23cc20533d9b3f51fe143ffbf7135da14b9ba
K.Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2022-12-01 14:20:22 +0530

    Create Pattern1.java
    Write a Python Program to solve the pattern in dynamic.
    
    Sample Input 1:
    5
    Sample Output 1:
    *****
    *****
    *****
    *****
    *****
    Sample Input 2:
    7
    Sample Output 2:
    *******
    *******
    *******
    *******
    *******
    *******
    *******

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pattern1
+{
+    public static void main(String[] args)
+    {
+        //Write your code
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+         for(int j=0;j<n;j++)
+        {
+            System.out.print("*");
+        
+        }
+        System.out.println("");
+        }
+    }
+}
