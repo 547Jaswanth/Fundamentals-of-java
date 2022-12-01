79c9f396370d3daab160fc53cbc58511f8f60231
K.Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2022-12-01 14:13:48 +0530

    Create Series4.java
    Write a program to generate the following series --- 0.5,1.5,4.5,13.5,...
    
     Input format: 
    
    The input containing an integer which denotes 'n' 
    
    Output format: 
    
    Print the series and refer the sample output for formatting.
    
    Sample Input & Output:
    
    5
    
    0.5 1.5 4.5 13.5 40.5

---
+++ Series4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Series4
+{
+    public static void main(String[] args)
+    {
+        //Write your code
+        Scanner sc=new Scanner(System.in);
+        int n,i;
+        double sum=0.5;
+        n=sc.nextInt();
+        for(i=0;i<=n;i++)
+        {
+                   System.out.println(sum);
+
+        double b=sum*3;
+        sum=b;
+             }
+    }
+}
